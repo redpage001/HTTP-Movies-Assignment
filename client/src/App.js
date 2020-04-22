132c633aac5d542b7e112891044bd5efaa6cd286
Manuel De La Mora <Manuel@defaults-mbp.attlocal.net>
2020-04-22 16:01:29 -0700

    Completed mvp creating update movie page and delete button using axios put and delete. Working on stretch of styling the application

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import UpdateMovie from "./components/UpdateMovie";
 import axios from 'axios';
 
 const App = () => {
@@ -22,7 +23,7 @@ const App = () => {
 
   useEffect(() => {
     getMovieList();
-  }, []);
+  }, [movieList]);
 
   return (
     <>
@@ -33,8 +34,13 @@ const App = () => {
       </Route>
 
       <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
+        <Movie addToSavedList={addToSavedList} movieList={movieList} setMovieList={setMovieList} />
       </Route>
+
+      <Route 
+      path="/update-movie/:id" 
+      render={() => <UpdateMovie movieList={movieList} setMovieList={setMovieList} />}
+      />
     </>
   );
 };
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
+function Movie(props) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const { push } = useHistory();
 
   const fetchMovie = (id) => {
     axios
@@ -15,7 +16,7 @@ function Movie({ addToSavedList }) {
   };
 
   const saveMovie = () => {
-    addToSavedList(movie);
+    props.addToSavedList(movie);
   };
 
   useEffect(() => {
@@ -26,13 +27,41 @@ function Movie({ addToSavedList }) {
     return <div>Loading movie information...</div>;
   }
 
+  const deleteMovie = e => {
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:5000/api/movies/${movie.id}`)
+      .then(res => {
+        // res.data ==> just the id
+        console.log({ res })
+        console.log({ movie })
+        const newMovies = props.movieList.filter(v => `${v.id}` !== movie.id)
+        props.setMovieList(newMovies)
+        console.log({ props })
+        push('/');
+      })
+      .catch(err => console.log(err));
+  }
+
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie} />
+      <div className="button-wrapper">
 
-      <div className="save-button" onClick={saveMovie}>
+      <button className="save-button" onClick={saveMovie}>
         Save
+      </button>
+
+      <button className="update-button" onClick={() => push(`/update-movie/${movie.id}`)}>
+        Update
+      </button>
+
+      <button className="delete-button" onClick={deleteMovie}>
+        Delete
+      </button>
+      
       </div>
+      
     </div>
   );
 }
